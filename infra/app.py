3787eafe060b6590bdc6dbf9df1c1282a87c2c66
Evan McKnight <emcknight@captechventures.com>
2025-11-26 01:40:06 -0500

    env variables pulled out, update requirements, update gitignore

--- infra/app.py
+++ infra/app.py
@@ -1,11 +1,15 @@
-# infra/app.py
 from aws_cdk import App, Environment
 from app_stack import AppStack
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = App()
 
-ACCOUNT_ID = app.node.try_get_context("account_id") or "783188002985"
-REGION = app.node.try_get_context("region") or "us-east-1"
+ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+REGION = os.getenv("REGION")
 
 env = Environment(account=ACCOUNT_ID, region=REGION)
 
