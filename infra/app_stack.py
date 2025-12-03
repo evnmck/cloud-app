e91dd6074f4023c73970c2df65d09ffcac1312a1
Evan McKnight <emcknight@captechventures.com>
2025-12-03 13:42:10 -0500

    add temp front end

--- infra/app_stack.py
+++ infra/app_stack.py
@@ -7,6 +7,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_lambda_event_sources as lambda_events,
 )
 import os
 from constructs import Construct
@@ -41,6 +42,21 @@ class AppStack(Stack):
             auto_delete_objects=True if stage == "dev" else False,
         )
 
+        upload_bucket.add_cors_rule(
+            allowed_methods=[
+                s3.HttpMethods.PUT,
+                s3.HttpMethods.GET,
+                s3.HttpMethods.HEAD,
+            ],
+            allowed_origins=[
+                "http://localhost:5173",  # your dev frontend
+                # later you can add your prod origin here, e.g. "https://app.my-domain.com"
+            ],
+            allowed_headers=["*"],  # or ["Content-Type"] if you want to be strict
+            exposed_headers=["ETag"],
+            max_age=3000,
+        )
+
         # ---------- Lambda: API ----------
         api_lambda = _lambda.Function(
             self,
@@ -71,6 +87,13 @@ class AppStack(Stack):
                 throttling_rate_limit=100,
                 throttling_burst_limit=50,
             ),
+            default_cors_preflight_options=apigw.CorsOptions(
+            # For dev, this is fine. You can restrict later if you want.
+            allow_origins=["http://localhost:5173"],
+            # Or: allow_origins=apigw.Cors.ALL_ORIGINS,
+            allow_methods=["GET", "POST", "PUT", "OPTIONS"],
+            allow_headers=["Content-Type", "X-API-TOKEN"],
+            ),
         )
 
         # Root /uploads -> POST
@@ -93,4 +116,26 @@ class AppStack(Stack):
         health_res.add_method(
             "GET",
             apigw.LambdaIntegration(api_lambda),
+        )
+
+        #---------- Update Lambda -------------
+        upload_handler = _lambda.Function(
+            self,
+            "UploadEventHandler",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="upload_handler.handler",
+            code=_lambda.Code.from_asset("../backend/pipeline"),  # for example
+            environment={
+                "JOBS_TABLE_NAME": jobs_table.table_name,
+            },
+        )
+
+        jobs_table.grant_read_write_data(upload_handler)
+
+        upload_handler.add_event_source(
+            lambda_events.S3EventSource(
+                upload_bucket,
+                events=[s3.EventType.OBJECT_CREATED_PUT],
+                filters=[s3.NotificationKeyFilter(prefix="uploads/")],
+            )
         )
